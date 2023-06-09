e258e2c8f991e70f3f10b3f5947b1f4a28bf875a
Haldies Gerhardien Pasya <81854080+haldies@users.noreply.github.com>
2023-06-09 23:07:05 +0700

    Add files via upload

--- index.py
+++ index.py
@@ -1,36 +1,22 @@
-# def enkripsi(plaintext, key):
-#     ciphertext = ""
-#     key = key.upper()
-#     key_len = len(key)
-#     for i in range(len(plaintext)):
-#         # Ubah huruf plaintext dan kunci menjadi bilangan 0-25
-#         plaintext_num = ord(plaintext[i]) - ord('A')
-#         key_num = ord(key[i % key_len]) - ord('A')
-#         # Enkripsi bilangan dengan teknik Vigenere
-#         ciphertext_num = (plaintext_num + key_num) % 26
-#         # Ubah bilangan menjadi huruf terenkripsi
-#         ciphertext += chr(ciphertext_num + ord('A'))
-#     return ciphertext
+# Materi decrypt and enkripsi Vigenere
 
-# def deskripsi(ciphertext, key):
-#     plaintext = ""
-#     key = key.upper()
-#     key_len = len(key)
-#     for i in range(len(ciphertext)):
-#         # Ubah huruf ciphertext dan kunci menjadi bilangan 0-25
-#         ciphertext_num = ord(ciphertext[i]) - ord('A')
-#         key_num = ord(key[i % key_len]) - ord('A')
-#         # Dekripsi bilangan dengan teknik Vigenere
-#         plaintext_num = (ciphertext_num - key_num) % 26
-#         # Ubah bilangan menjadi huruf plainteks
-#         plaintext += chr(plaintext_num + ord('A'))
-#     return plaintext
-# plaintext = input("Masukan plaintext : ")
-# key = input("Masukan key: ")
-# ciphertext = enkripsi(plaintext, key)
-# print("Ciphertext:", ciphertext)
-# deskripsi_text = deskripsi(ciphertext, key)
-# print("Decrypted text:", deskripsi_text)
+def enkripsi(plaintext, key):
+    ciphertext = ""
+    key = key.upper()
+    key_len = len(key)
+    for i in range(len(plaintext)):
+        # Ubah huruf plaintext dan kunci menjadi bilangan 0-25
+        plaintext_num = ord(plaintext[i]) - ord('A')
+        key_num = ord(key[i % key_len]) - ord('A')
+        # Enkripsi bilangan dengan teknik Vigenere
+        ciphertext_num = (plaintext_num + key_num) % 26
+        # Ubah bilangan menjadi huruf terenkripsi
+        ciphertext += chr(ciphertext_num + ord('A'))
+    return ciphertext
+plaintext = input("Masukan plaintext : ")
+key = input("Masukan key: ")
+ciphertext = enkripsi(plaintext, key)
+print("Ciphertext:", ciphertext)
 
 def decrypt_vigenere(ciphertext, key):
     plaintext = ""
